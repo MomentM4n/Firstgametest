b5349bf5464e7ad870a91e23d840549829dd079e
MarkoMatko <marko.matkovic95@gmail.com>
2016-11-04 19:05:59 +0100

    Added Sniper and cursor setVisible(false)

--- Moorhuhn/src/main/Test.java
+++ Moorhuhn/src/main/Test.java
@@ -1,10 +1,14 @@
 package main;
 
+import gui.AppFrame;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		AppFrame app = new AppFrame();
+		app.initGameWindow();
+		
 	}
 
 }
