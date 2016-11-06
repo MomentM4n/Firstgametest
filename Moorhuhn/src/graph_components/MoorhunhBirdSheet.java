c4eb09408aa34752a97d015e468086f27d1d0c95
pendula95 <Deathbringer123>
2016-11-06 19:45:41 +0100

    Inver Sheet

--- Moorhuhn/src/graph_components/MoorhunhBirdSheet.java
+++ Moorhuhn/src/graph_components/MoorhunhBirdSheet.java
@@ -2,6 +2,8 @@ package graph_components;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 
 import javax.swing.JOptionPane;
 
@@ -10,6 +12,7 @@ import rafgfxlib.Util;
 public class MoorhunhBirdSheet {
 
 	private BufferedImage sheet = null;
+	private BufferedImage invertSheet = null;
 	private int frameW, frameH;
 	private int sheetW, sheetH;
 	private int offsetX = 0, offsetY = 0;
@@ -56,6 +59,46 @@ public class MoorhunhBirdSheet {
 	public void setOffsets(int x, int y){
 		offsetX = x;
 		offsetY = y;
+	}
+	
+	/*public void invertSheet() {
+		WritableRaster source = sheet.getRaster();
+		System.out.println(source.getWidth() + ":" + source.getHeight());
+		WritableRaster target = Util.createRaster(source.getWidth(), source.getHeight(), false);
+		int rgb[] = new int[4];
+		
+		for(int y = 0; y < source.getHeight(); y++) {
+			for(int x = 0; x < source.getWidth(); x++) {
+				
+				source.getPixel(x, y, rgb);
+				
+				// Negativ slike dobijamo radeci komplement svih komponenti,
+				// odnosno, novoR = 255 - staroR, itd, gdje ce onda (0,0,0)
+				// postati (255,255,255), itd, sto i ocekujemo.
+				rgb[0] = 255 - rgb[0];
+				rgb[1] = 255 - rgb[1];
+				rgb[2] = 255 - rgb[2];
+				rgb[3] = 255 - rgb[3];
+				
+				target.setPixel(x, y, rgb);
+			}
+		}
+		sheet = Util.rasterToImage(target);
+	}*/
+	
+	public void invertSheet() {
+		for(int y = 0; y < sheet.getHeight(); y++) {
+			for(int x = 0; x < sheet.getWidth(); x++) {
+				int pixel = sheet.getRGB(x, y);
+				int neg = 0xFFFFFF - pixel;
+				int neg1 = pixel;
+				if(! ((neg1>>24) == 0x00) ) {
+
+					neg1 = (0xFFFFFF - pixel) | 0xFF000000;
+				}
+				sheet.setRGB(x, y, neg1);
+			}
+		}
 	}
 	
 	public void setOffsetX(int x) { offsetX = x; }
